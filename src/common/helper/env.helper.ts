1ec4d90bbcaf8d55b037d6564b12ecef216df2a1
Kruy Vanna <kruyvanna@gmail.com>
2023-02-27 16:40:40 +0700

    [ENV] switching development, staging, and production

--- src/common/helper/env.helper.ts
+++ src/common/helper/env.helper.ts
@@ -2,15 +2,30 @@ import { existsSync } from 'fs';
 import { resolve } from 'path';
 
 export function getEnvPath(dest: string): string {
-  const env: string | undefined = process.env.NODE_ENV;
   const fallback: string = resolve(`${dest}/.env`);
 
-  console.log('process.env.ENV :', process.env.ENV);
-  if (process.env.ENV == 'prod') {
-    console.log('== "prod" :', 'YES');
+  let fileName = '';
+  switch (process.env.ENV) {
+    case 'dev': {
+      fileName = 'development.env';
+      break;
+    }
+
+    case 'staging': {
+      fileName = 'staging.env';
+      break;
+    }
+
+    case 'prod': {
+      fileName = 'production.env';
+      break;
+    }
+
+    case undefined: {
+      fileName = 'development.env';
+    }
   }
-  const filename: string = env ? `${env}.env` : 'development.env';
-  let filePath: string = resolve(`${dest}/${filename}`);
+  let filePath: string = resolve(`${dest}/${fileName}`);
 
   if (!existsSync(filePath)) {
     filePath = fallback;
